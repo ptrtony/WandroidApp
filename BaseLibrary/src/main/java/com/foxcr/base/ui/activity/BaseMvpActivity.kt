336a843814b141e1e69cf9dd35ec16757fd4de9d
ptrtony <1029651482@qq.com>
2020-01-21 23:14:49 +0800

    新增网络加载进度条

--- BaseLibrary/src/main/java/com/foxcr/base/ui/activity/BaseMvpActivity.kt
+++ BaseLibrary/src/main/java/com/foxcr/base/ui/activity/BaseMvpActivity.kt
@@ -1,15 +1,22 @@
 package com.foxcr.base.ui.activity
-
 import com.foxcr.base.presenter.BasePresenter
 import com.foxcr.base.presenter.view.BaseView
+import com.foxcr.base.widgets.LoadingDialog
 
 open class BaseMvpActivity<T:BasePresenter<*>> : BaseActivity(),BaseView {
+    private val mLoadingDialog: LoadingDialog by lazy {
+        LoadingDialog(this)
+    }
     override fun showLoading() {
-
+        if (!mLoadingDialog.isShowing){
+            mLoadingDialog.show()
+        }
     }
 
     override fun hideLoading() {
-
+        if (mLoadingDialog.isShowing){
+            mLoadingDialog.cancel()
+        }
     }
 
     override fun onError() {
@@ -18,4 +25,10 @@ open class BaseMvpActivity<T:BasePresenter<*>> : BaseActivity(),BaseView {
 
     lateinit var mPresenter:T
 
+    override fun onDestroy() {
+        super.onDestroy()
+        if (mLoadingDialog.isShowing){
+            mLoadingDialog.cancel()
+        }
+    }
 }
